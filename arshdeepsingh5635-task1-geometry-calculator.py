cafe75899e0cc9f44fcbde9acf0eb627af6b02c4
Arshdeep Singh <99043392+Arsh4844@users.noreply.github.com>
2022-03-08 19:26:38 -0500

    Update arshdeepsingh5635-task1-geometry-calculator.py

--- arshdeepsingh5635-task1-geometry-calculator.py
+++ arshdeepsingh5635-task1-geometry-calculator.py
@@ -1,9 +1,9 @@
-from xml.sax.saxutils import prepare_input_source
-
-
-length = input("Enter Length of Rectangle in cm's :- ")
-width = input("Enter width of Rectangle in cm's :- ")
-area = float(length) * float(width)
-perimeter = 2*float(length + width)
-print("Area = " + str(area) + " cm sq")
-print("Perimeter = "+ str(perimeter) + " cm")
+#Author = Arshdeep Singh
+#Date created = january 31, 2022
+#Date Modified = January 31, 2022
+length = input("Enter Length of Rectangle in cm's :- ")#length input asked from user
+width = input("Enter width of Rectangle in cm's :- ")#width input asked form user
+area = float(length) * float(width)#rectangle area formula used with float function as input can be decimal
+perimeter = 2*float(length + width)#perimeter calculated with common float function for length and width
+print("Area = " + str(area) + " cm sq")#Area printed
+print("Perimeter = "+ str(perimeter) + " cm")#perimeter printed
